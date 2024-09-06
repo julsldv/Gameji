10bfe8b4ccc8bd2efcd6f3c48993262fbbdf8153
Julien PORTOLAN <julsldv@gmail.com>
2024-09-06 13:00:06 +0200

    Initial Commit

---
+++ Gameji/GamejiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GamejiApp.swift
+//  Gameji
+//
+//  Created by Julien PORTOLAN on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct GamejiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
